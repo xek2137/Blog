46b49913149553b3bfad06db549c06616ee28350
Adrian Piech <adrianpiech.git@gmail.com>
2021-04-12 16:11:17 +0200

    correct handler event

--- js/script.js
+++ js/script.js
@@ -1,37 +1,32 @@
 {
     'use strict';
-    
-    /*document.getElementById('test-button').addEventListener('click', function(){
-        const links = document.querySelectorAll('.titles a');
-        console.log('links:', links);
-      })*/
-    
+
     const titleClickHandler = function (event) {
         event.preventDefault();
         const clickedElement = this;
         console.log('Link was clicked!');
-    
+
         /* remove class 'active' from all article links  */
 
         const activeLinks = document.querySelectorAll('.titles a.active');
-    
+
         for (let activeLink of activeLinks) {
             activeLink.classList.remove('active');
         }
-    
+
         /* add class 'active' to the clicked link */
-    
+
         clickedElement.classList.add('active');
         console.log('clickedElement', clickedElement);
-    
+
         /* remove class 'active' from all articles */
-    
+
         const activeArticles = document.querySelectorAll('.post.active');
-    
+
         for (let activeArticle of activeArticles) {
             activeArticle.classList.remove('active');
         }
-    
+
         /* get 'href' attribute from the clicked link */
 
         const articleSelector = clickedElement.getAttribute('href');
@@ -39,16 +34,15 @@
         /* find the correct article using the selector (value of 'href' attribute) */
 
         const targetArticle = document.querySelector(articleSelector);
-    
+
         /* add class 'active' to the correct article */
 
         targetArticle.classList.add('active');
     }
-    
+
     const links = document.querySelectorAll('.titles a');
-    
+
     for (let link of links) {
         link.addEventListener('click', titleClickHandler);
     }
-    
 }
